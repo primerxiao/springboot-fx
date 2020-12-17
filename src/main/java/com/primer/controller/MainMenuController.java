7ac8e7962b9cfe655d017c86516181fd77a27d35
肖均辉 <primerxiao@163.com>
2020-12-17 23:18:00 +0800

    init code

--- src/main/java/com/primer/controller/MainMenuController.java
+++ src/main/java/com/primer/controller/MainMenuController.java
@@ -1,5 +1,7 @@
 package com.primer.controller;
 
+import com.jfoenix.controls.JFXRippler;
+import com.jfoenix.effects.JFXDepthManager;
 import com.primer.MainApplication;
 import com.primer.jfxsupport.AbstractFxmlView;
 import com.primer.jfxsupport.FXMLController;
@@ -8,6 +10,10 @@ import com.primer.view.VersionHelperView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 
 import java.net.URL;
@@ -16,30 +22,74 @@ import java.util.ResourceBundle;
 @FXMLController
 public class MainMenuController implements Initializable {
 
+    public FlowPane flowPane;
+
+    private static final String FX_BACKGROUND_COLOR_WHITE = "-fx-background-color:WHITE;";
+    private static int counter = 0;
+    private static int step = 1;
+    public StackPane stackPane;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //TODO drop shadow changes the width and height thus need to be considered
+        flowPane.setVgap(20);
+        flowPane.setHgap(20);
 
-    }
+        Label label = new Label("Click Me");
+        label.setStyle(FX_BACKGROUND_COLOR_WHITE);
+        label.setPadding(new Insets(20));
+        JFXRippler rippler = new JFXRippler(label);
+        rippler.setEnabled(false);
+        flowPane.getChildren().add(rippler);
 
-    @FXML
-    public void openBatchHelper(ActionEvent actionEvent) {
-        MainApplication.showInitialView(BatchHelperView.class);
-    }
+        label.setOnMousePressed((e) -> {
+            if (counter == 5) {
+                step = -1;
+            } else if (counter == 0) {
+                step = 1;
+            }
+            JFXDepthManager.setDepth(label, counter += step % JFXDepthManager.getLevels());
+        });
 
-    @FXML
-    public void openLogHelper(ActionEvent actionEvent) throws ClassNotFoundException {
-        Class<?> aClass = Class.forName("com.primer.view.LogHelperView");
-        MainApplication.showInitialView((Class<? extends AbstractFxmlView>) aClass);
-    }
+        Label l1 = new Label("TEST");
+        l1.setStyle(FX_BACKGROUND_COLOR_WHITE);
+        l1.setPadding(new Insets(20));
+        JFXRippler rippler1 = new JFXRippler(l1);
+        flowPane.getChildren().add(rippler1);
+        JFXDepthManager.setDepth(rippler1, 1);
 
-    @FXML
-    public void openEsbHelper(ActionEvent actionEvent) {
+        Label l2 = new Label("TEST1");
+        l2.setStyle(FX_BACKGROUND_COLOR_WHITE);
+        l2.setPadding(new Insets(20));
+        JFXRippler rippler2 = new JFXRippler(l2);
+        flowPane.getChildren().add(rippler2);
+        JFXDepthManager.setDepth(rippler2, 2);
 
-    }
 
-    @FXML
-    public void openVersionHelper(ActionEvent actionEvent) {
-        MainApplication.showView(VersionHelperView.class,Modality.NONE);
+        Label l3 = new Label("TEST2");
+        l3.setStyle(FX_BACKGROUND_COLOR_WHITE);
+        l3.setPadding(new Insets(20));
+        JFXRippler rippler3 = new JFXRippler(l3);
+        flowPane.getChildren().add(rippler3);
+        JFXDepthManager.setDepth(rippler3, 3);
+
+        Label l4 = new Label("TEST3");
+        l4.setStyle(FX_BACKGROUND_COLOR_WHITE);
+        l4.setPadding(new Insets(20));
+        JFXRippler rippler4 = new JFXRippler(l4);
+        flowPane.getChildren().add(rippler4);
+        JFXDepthManager.setDepth(rippler4, 4);
+
+        Label l5 = new Label("TEST4");
+        l5.setStyle(FX_BACKGROUND_COLOR_WHITE);
+        l5.setPadding(new Insets(20));
+        JFXRippler rippler5 = new JFXRippler(l5);
+        flowPane.getChildren().add(rippler5);
+        JFXDepthManager.setDepth(rippler5, 5);
+
+        StackPane.setMargin(flowPane, new Insets(100));
+        stackPane.setStyle("-fx-background-color:WHITE");
+
     }
 
 }
